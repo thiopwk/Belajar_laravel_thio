3b40d959f29b713e52cf298650265630df372fc3
dhikaxzz <afdhikasyahputra@gmail.com>
2023-10-07 22:52:20 +0700

    menambah seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // fitur ajaib untuk meng insert 10 database acak
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+            
+            
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,13 +6,20 @@
         <h1 class="mb-5">Halaman Blog Posts</h1>
     {{-- posts dalam web.php diubah menjadi $post lalu akan terjadi pengulangan--}}
     @foreach ($posts as $post)
-    <article class="mb-5">
+    <article class="mb-5 border-bottom pb-4">
         <h2>
             {{-- /posts/ diambil dari posts.blade.php (WAJIB SESUAI) lalu akan di arahkan ke halaman posts single yang sudah dibuat di web.php--}}
             {{-- post id terhubung ke database --}}
-            <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
         </h2>
+
+        <p>By : Afdhika Syahputra in <a href="/categories/{{$post->category->slug}}
+        "class="text-decoration-none">{{ $post->category->name }}</a></p>
+
         <p>{{ $post->excerpt }}</p>
+
+        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Lihat Lebih Lanjut</a>
+
     </article>
         @endforeach
 
