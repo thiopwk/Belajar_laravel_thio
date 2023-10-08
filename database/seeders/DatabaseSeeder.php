d3f7208093bba3852650d35999fc7e7b24974aa8
dhikaxzz <afdhikasyahputra@gmail.com>
2023-10-08 09:06:37 +0700

    menambah halaman author

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +23,57 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
             
-            
+        User::create([
+            'name' => 'Afdhika Syahputra',
+            'email' => 'afdhika@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quaerat numquam provident sapiente quos repellendus nesciunt eligendi deserunt deleniti asperiores.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aperiam ex a itaque deserunt voluptatem, tenetur velit quas error repudiandae dicta ipsa excepturi fugiat, nisi alias repellendus distinctio! Saepe recusandae quo enim unde voluptates sint! Porro mollitia soluta, pariatur sed natus eum nihil est iure reprehenderit id rerum voluptatum qui modi at exercitationem saepe cupiditate dignissimos ab repellat veritatis animi voluptas! Excepturi ut impedit eligendi inventore, debitis est sequi repellat eaque, eius libero fugit in enim sunt earum repudiandae qui perspiciatis minima voluptatum odit accusamus dignissimos. Quisquam rerum ducimus beatae deserunt excepturi ipsum esse, nulla ea officia maxime sunt nisi!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quaerat numquam provident sapiente quos repellendus nesciunt eligendi deserunt deleniti asperiores.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aperiam ex a itaque deserunt voluptatem, tenetur velit quas error repudiandae dicta ipsa excepturi fugiat, nisi alias repellendus distinctio! Saepe recusandae quo enim unde voluptates sint! Porro mollitia soluta, pariatur sed natus eum nihil est iure reprehenderit id rerum voluptatum qui modi at exercitationem saepe cupiditate dignissimos ab repellat veritatis animi voluptas! Excepturi ut impedit eligendi inventore, debitis est sequi repellat eaque, eius libero fugit in enim sunt earum repudiandae qui perspiciatis minima voluptatum odit accusamus dignissimos. Quisquam rerum ducimus beatae deserunt excepturi ipsum esse, nulla ea officia maxime sunt nisi!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quaerat numquam provident sapiente quos repellendus nesciunt eligendi deserunt deleniti asperiores.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aperiam ex a itaque deserunt voluptatem, tenetur velit quas error repudiandae dicta ipsa excepturi fugiat, nisi alias repellendus distinctio! Saepe recusandae quo enim unde voluptates sint! Porro mollitia soluta, pariatur sed natus eum nihil est iure reprehenderit id rerum voluptatum qui modi at exercitationem saepe cupiditate dignissimos ab repellat veritatis animi voluptas! Excepturi ut impedit eligendi inventore, debitis est sequi repellat eaque, eius libero fugit in enim sunt earum repudiandae qui perspiciatis minima voluptatum odit accusamus dignissimos. Quisquam rerum ducimus beatae deserunt excepturi ipsum esse, nulla ea officia maxime sunt nisi!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quaerat numquam provident sapiente quos repellendus nesciunt eligendi deserunt deleniti asperiores.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aperiam ex a itaque deserunt voluptatem, tenetur velit quas error repudiandae dicta ipsa excepturi fugiat, nisi alias repellendus distinctio! Saepe recusandae quo enim unde voluptates sint! Porro mollitia soluta, pariatur sed natus eum nihil est iure reprehenderit id rerum voluptatum qui modi at exercitationem saepe cupiditate dignissimos ab repellat veritatis animi voluptas! Excepturi ut impedit eligendi inventore, debitis est sequi repellat eaque, eius libero fugit in enim sunt earum repudiandae qui perspiciatis minima voluptatum odit accusamus dignissimos. Quisquam rerum ducimus beatae deserunt excepturi ipsum esse, nulla ea officia maxime sunt nisi!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
     }
 }
