3e05c1dab06493ec6dd8697da8fc090a57fd3256
dhikaxzz <afdhikasyahputra@gmail.com>
2023-10-08 18:11:34 +0700

    memperbaki halaman category

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function index()
     {
         return view('posts',[
-            "title" => "Posts",
+            "title" => "All Posts",
             // "posts" => Post::all()
             "posts" => Post::latest()->get()
         ]);
