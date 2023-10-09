4f0845e32080262b20cade4d95cd7b2599fe9dea
dhikaxzz <afdhikasyahputra@gmail.com>
2023-10-09 20:56:26 +0700

    memperbaiki single post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -42,6 +42,7 @@
         @foreach ($posts->skip(1) as $post)
         <div class="col-md-4 mb-3">
             <div class="card">
+                <div class="position-absolute px-3 py-2 fs-6" style="background-color: rgba(102, 94, 94, 0.7)"><a href="/categories/{{$post->category->slug}}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
                 <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
                 <div class="card-body">
                   <h5 class="card-title">{{ $post->title }}</h5>
@@ -50,7 +51,7 @@
                         By : <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">
                         {{$post->author->name}}</a> {{ $post->created_at->diffForHumans() }}</small></p>
                   <p class="card-text">{{ $post->excerpt }}</p>
-                  <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Lihat Lebih Lanjut</a>
+                  <a href="/posts/{{ $post->slug }}" class="btn btn-success">Lihat Lebih Lanjut</a>
                 </div>
               </div>
             </div>
