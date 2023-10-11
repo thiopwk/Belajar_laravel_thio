60b48c6bc8994625ca62054ef80ad3a6019e1ba6
dhikaxzz <afdhikasyahputra@gmail.com>
2023-10-11 17:13:19 +0700

    mengatasi eror validasi

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -12,20 +12,43 @@
         @csrf
 
         <div class="form-floating">
-          <input type="text" name="name" class="form-control rounded-top" id="name" placeholder="name@example.com">
+          <input type="text" name="name" class="form-control rounded-top 
+          {{-- jika name eror kasih class invalid --}}
+          @error('name')is-invalid @enderror" id="name" placeholder="name@example.com" required value="{{ old('name') }}">
           <label for="name">Name</label>
+          {{-- jika eror maka jalankan invalid feedback --}}
+          @error('name')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+          @enderror
         </div>
         <div class="form-floating">
-          <input type="text" name="username" class="form-control" id="username" placeholder="username@example.com">
+          <input type="text" name="username" class="form-control @error('username')is-invalid @enderror" id="username" placeholder="username@example.com" required {{ old('username') }}>
           <label for="username">Username</label>
+          @error('username')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+          @enderror
         </div>
         <div class="form-floating">
-          <input type="email" name="email" class="form-control" id="email" placeholder="email@example.com">
+          <input type="email" name="email" class="form-control @error('username')is-invalid @enderror" id="email" placeholder="email@example.com" required {{ old('email') }}>
           <label for="email">Email Address</label>
+          @error('email')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+          @enderror
         </div>
         <div class="form-floating">
-          <input type="password" name="password" class="form-control rounded-bottom" id="password" placeholder="Password">
+          <input type="password" name="password" class="form-control rounded-bottom @error('username')is-invalid @enderror" id="password" placeholder="Password" required>
           <label for="password">Password</label>
+          @error('password')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+          @enderror
         </div>
     
         <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">Register</button>
