9ee1699a79226036789fd1ce8fabd2a990f87c40
dhikaxzz <afdhikasyahputra@gmail.com>
2023-10-13 15:55:33 +0700

    feather icon

---
+++ resources/views/dashboard/layouts/main.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Afdhika Syahputra & Thio Putra  Collaboration">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>WPU BLOG | DASHBOARD</title>
+    
+    <!-- Bootstrap core CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="/css/dashboard.css" rel="stylesheet">
+  </head>
+  <body>
+    {{-- memanggil file --}}
+@include('dashboard.layouts.header')
+
+<div class="container-fluid">
+  <div class="row">
+
+    {{-- memanggil file --}}
+  @include('dashboard.layouts.sidebar')
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      @yield('container')
+    </main>
+  </div>
+</div>
+
+
+{{-- JAVASCRIPT BOOTSTRAP --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+      <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+
+      <script src="/js/dashboard.js"></script>
+  </body>
+</html>
